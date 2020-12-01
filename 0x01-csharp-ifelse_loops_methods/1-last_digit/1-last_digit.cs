ca92d299fb533f500d62f82d556766d7e10c0bc2
Mitchell <mmoscovics@yahoo.com>
2020-12-01 16:59:46 -0500

    [Create] last digit

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        string str = "The last digit of ";
+        int ld = number % 10;
+        if (ld > 5)
+            Console.WriteLine($"{str}{number} is {ld} and is greater than 5");
+        else if (ld < 6 && !(ld == 0))
+            Console.WriteLine($"{str}{number} is {ld} and is less than 6 and not 0");
+        else
+            Console.WriteLine($"{str}{number} is {ld} and is 0");
+    }
+}
