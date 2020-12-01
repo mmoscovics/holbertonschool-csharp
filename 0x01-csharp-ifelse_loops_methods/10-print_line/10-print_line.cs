91905320b7acceafee2a419a73ab704791062870
Mitchell <mmoscovics@yahoo.com>
2020-12-01 17:23:10 -0500

    [Create] print line

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length < 0)
+            Console.WriteLine("");
+        for (int line = 0; line < length; line++)
+            Console.Write("_");
+        Console.WriteLine("");
+    }
+}
