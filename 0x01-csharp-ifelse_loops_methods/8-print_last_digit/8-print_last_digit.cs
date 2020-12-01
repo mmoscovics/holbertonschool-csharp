5bd19028fd419050f2d62f9e0e201c3afd950cc5
Mitchell <mmoscovics@yahoo.com>
2020-12-01 17:17:16 -0500

    [Create] print last digit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int ld = Math.Abs(number) % 10;
+        Console.Write($"{ld}");
+        return ld;
+    }
+}
