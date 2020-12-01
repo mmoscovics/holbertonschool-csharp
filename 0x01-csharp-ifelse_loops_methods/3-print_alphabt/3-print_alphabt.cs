7d69440ef4a2d43fc714868102f2cd1aab5b163a
Mitchell <mmoscovics@yahoo.com>
2020-12-01 17:02:55 -0500

    [Create] alphabt loop

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            {
+                if (alphabet == 'q' || alphabet == 'e')
+                    continue;
+                else
+                    Console.Write(alphabet);
+            }
+        }
+    }
+}
