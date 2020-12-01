1296dec1c0caebe41510a7d5f32d041153a7d593
Mitchell <mmoscovics@yahoo.com>
2020-12-01 17:04:05 -0500

    [Create] print hex

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numb = 0; numb < 99; numb++)
+                Console.Write($"{numb} = 0x{numb:x}\n");
+        }
+    }
+}
