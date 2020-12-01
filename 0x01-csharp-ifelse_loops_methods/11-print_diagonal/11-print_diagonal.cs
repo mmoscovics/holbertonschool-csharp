55f5aac4352902041aa3d4e7060ac02361a189ee
Mitchell <mmoscovics@yahoo.com>
2020-12-01 17:28:32 -0500

    [Create] print diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+            Console.WriteLine("");
+        for (int line = 0; line < length; line++)
+        {
+            for (int space = 0; space < line; space++)
+                Console.Write(" ");
+            Console.WriteLine("\\");
+        }
+    }
+}
