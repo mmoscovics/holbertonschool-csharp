437140e8d13c64f443d4ec0e4e38c69b0ccd9afd
Mitchell <mmoscovics@yahoo.com>
2020-12-01 17:16:44 -0500

    [Create] print combination 2

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = 0;
+            for (numb = 1; numb < 89; numb++)
+            {
+                if (numb < 89 && numb / 10 < numb % 10)
+                    Console.Write($"{numb:00}, ");
+            }
+            Console.Write($"{numb:00}\n");
+        }
+    }
+}
