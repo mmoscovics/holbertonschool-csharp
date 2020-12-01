0cc99174e4cea55121d871b9c7debf673872cc63
Mitchell <mmoscovics@yahoo.com>
2020-12-01 17:16:30 -0500

    [Create] print combination

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numb = 0; numb < 100; numb++)
+                if (numb < 99)
+                    Console.Write($"{numb:00}, ");
+                else
+                    Console.Write($"{numb}\n");
+        }
+    }
+}
