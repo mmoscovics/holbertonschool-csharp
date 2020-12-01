0105c8f67fda07f1ca2e04d4ccc65f803ac5f13a
Mitchell <mmoscovics@yahoo.com>
2020-12-01 17:36:09 -0500

    [Create] fizz buzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num = 1; num <= 100; num++)
+        {
+            if (num % 15 == 0)
+                Console.Write("FizzBuzz");
+            else if (num % 5 == 0)
+                Console.Write("Buzz");
+            else if (num % 3 == 0)
+                Console.Write("Fizz");
+            else
+                Console.Write(num);
+            if (num < 100)
+                Console.Write(" ");
+            else
+                Console.WriteLine("");
+        }
+    }
+}
