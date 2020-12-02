a7ea97fb73d56c91e020d6d24c1b00bdc8e2ad94
Mitchell <mmoscovics@yahoo.com>
2020-12-02 09:48:22 -0500

    [Create] list bool

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> new_list = new List<bool>();
+        for (int i = 0; i < myList.Count; i ++)
+            if (myList[i] % 2 == 0)
+                new_list.Add(true);
+            else
+                new_list.Add(false);
+        return new_list;
+    }
+}
