60c2502e6b1dc6e3f48a8d3e08cfbd8a02c9688a
Mitchell <mmoscovics@yahoo.com>
2020-12-02 10:38:44 -0500

    [Create] delete at

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        for (int i = 0; i < myList.Count; i++)
+            if (i == index)
+                myList.Remove(myList[i]);
+        return myList;
+    }
+}
