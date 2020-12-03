bc20fd2b740223cd265f5460b84ec863b2dd712f
Mitchell <mmoscovics@yahoo.com>
2020-12-02 19:07:16 -0500

    [Create] dictionary multiply

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        return myDict.ToDictionary(pair => pair.Key, pair => pair.Value * 2);
+    }
+}
