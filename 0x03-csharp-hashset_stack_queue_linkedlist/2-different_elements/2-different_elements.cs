9e629c680d1f1c96311a378ae534c9d8a24fba5b
Mitchell <mmoscovics@yahoo.com>
2020-12-03 16:43:44 -0500

    [Create] differents list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> common_set = new HashSet<int>(list1);
+        common_set.SymmetricExceptWith(list2);
+        List<int> common_list = new List<int>(common_set);
+        common_list.Sort();
+        return common_list;
+    }
+}
