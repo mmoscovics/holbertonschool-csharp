1edd7d34d9950ade4c73d18b5c7a5f6cee57fd43
Mitchell <mmoscovics@yahoo.com>
2020-12-03 17:28:34 -0500

    [Create] queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        aQueue.Enqueue(newItem);
+        Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
+        if (aQueue.Contains(search))
+        {
+            int target = 0;
+            while (target == 0)
+            {
+                if (aQueue.Peek().Equals(search))
+                    target += 1;
+                aQueue.Dequeue();
+            }
+        }
+        return aQueue;
+    }
+}
