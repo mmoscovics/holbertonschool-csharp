14d3d337d473dfe626785d0ae0ef85afd93f50d2
Mitchell <mmoscovics@yahoo.com>
2020-12-02 19:54:35 -0500

    [Create] jagged array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged_array = new int [3][];
+            jagged_array[0] = new int[] { 0, 1, 2, 3};
+            jagged_array[1] = new int[] { 0, 1, 2, 3, 4, 5, 6};
+            jagged_array[2] = new int[] { 0, 1};
+            for (int i = 0; i < jagged_array.Length; i++)
+            {
+                for (int j = 0; j < jagged_array[i].Length; j++)
+                {
+                    Console.Write($"{jagged_array[i][j]}");
+                    if (j < jagged_array[i].Length - 1)
+                        Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
