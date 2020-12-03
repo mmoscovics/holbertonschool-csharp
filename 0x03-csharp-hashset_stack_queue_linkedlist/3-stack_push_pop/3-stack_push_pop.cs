bd61f7a9f05f37dbeb9f6c8ea361441cb136a440
Mitchell <mmoscovics@yahoo.com>
2020-12-03 17:08:01 -0500

    [Create] stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of Items: {aStack.Count}");
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
+        if (aStack.Contains(search))
+        {
+            int target = 0;
+            while (target == 0)
+            {
+                if (aStack.Peek().Equals(search))
+                    target += 1;
+                aStack.Pop();
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
