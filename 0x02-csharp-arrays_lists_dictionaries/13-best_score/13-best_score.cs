03b329527f572b1b2d41e98c23ec96e377801d2d
Mitchell <mmoscovics@yahoo.com>
2020-12-02 19:41:59 -0500

    [Create] max value by key

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count < 1)
+            return "None";
+        return myList.Aggregate((val1, val2) => val1.Value > val2.Value ? val1 : val2).Key;
+    }
+}
