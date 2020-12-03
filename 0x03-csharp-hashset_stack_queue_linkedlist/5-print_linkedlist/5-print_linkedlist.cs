d8fd283caf4ef7635eadffc65f9beb6930abdf57
Mitchell <mmoscovics@yahoo.com>
2020-12-03 17:40:25 -0500

    [Create] print linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> new_llist = new LinkedList<int>();
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine(i);
+            new_llist.AddLast(i);
+        }
+        return new_llist;
+    }
+}
