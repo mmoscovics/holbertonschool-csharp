1fd2180144b591a0ed4c8e0d5e7ddc96e9a9af95
Mitchell <mmoscovics@yahoo.com>
2020-12-02 19:14:13 -0500

    [Create] sort dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> key_list = myDict.Keys.ToList();
+        key_list.Sort();
+        foreach(string key in key_list)
+            Console.WriteLine($"{key}: {myDict[key]}");
+    }
+}
