b82346439ad3a6f4cd798020f003ed87a7792c34
Mitchell <mmoscovics@yahoo.com>
2020-12-03 17:11:42 -0500

    [Update] typo

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,7 +5,7 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine($"Number of Items: {aStack.Count}");
+        Console.WriteLine($"Number of items: {aStack.Count}");
         if (aStack.Count == 0)
             Console.WriteLine("Stack is empty");
         else
