adba458b44b8b8a70ff423e1fea40300210ffcb7
Mitchell <mmoscovics@yahoo.com>
2020-12-02 19:48:12 -0500

    [Create] 2d array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] td_array = new int[5, 5];
+            td_array[2, 2] = 1;
+            for (int i = 0; i < td_array.GetLength(0); i++)
+            {
+                for (int j = 0; j < td_array.GetLength(1); j++)
+                {
+                    Console.Write(td_array[i, j]);
+                    if (j < td_array.GetLength(1) - 1)
+                        Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
