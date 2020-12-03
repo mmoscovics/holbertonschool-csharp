144c08cd47d944fe21cdb07aae0eeb41012fe18c
Mitchell <mmoscovics@yahoo.com>
2020-12-03 16:28:57 -0500

    [Create] unique add

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> unique_numb = new HashSet<int>();
+        int total = 0;
+        foreach (int number in myList)
+            unique_numb.Add(number);
+        foreach (int i in unique_numb)
+            total += i;
+        return total;
+    }
+}
