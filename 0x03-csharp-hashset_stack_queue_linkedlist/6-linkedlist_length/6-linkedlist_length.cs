26bde27e157e199b5aba3a54c52d592678411822
Mitchell <mmoscovics@yahoo.com>
2020-12-03 18:17:20 -0500

    [Create] linkedlist length

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        foreach (int i in myLList)
+            count += 1;
+        return count;
+    }
+}
