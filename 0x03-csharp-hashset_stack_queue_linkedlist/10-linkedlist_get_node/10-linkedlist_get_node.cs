3f94d98b884a91856c8c30805479be94a19f003d
Mitchell <mmoscovics@yahoo.com>
2020-12-03 19:27:14 -0500

    [Create] index value

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        foreach (int i in myLList)
+        {
+            if (index == n)
+                return i;
+            index += 1;
+        }
+        return 0;
+    }
+}
