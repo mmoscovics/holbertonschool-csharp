b4371e3f1e1374cfac5385cc32047fc4ac30393f
Mitchell <mmoscovics@yahoo.com>
2020-12-03 19:05:17 -0500

    [Create] linkedlist find

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        foreach (int i in myLList)
+        {
+            if (i == value)
+                return index;
+            index += 1;
+        }
+        return -1;
+    }
+}
