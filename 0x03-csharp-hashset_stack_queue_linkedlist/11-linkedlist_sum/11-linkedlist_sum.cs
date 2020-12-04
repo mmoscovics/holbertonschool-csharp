a6af24ea12c7ba731a57e8922f0df6497d599edb
Mitchell <mmoscovics@yahoo.com>
2020-12-03 19:33:42 -0500

    [Create] sum linkedlist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int total = 0;
+        foreach (int i in myLList)
+            total += i;
+        return total;
+    }
+}
