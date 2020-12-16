dbc2eda1b7bc6b0fc6f0c6a9b7607024c726e56f
Mitchell <mmoscovics@yahoo.com>
2020-12-15 23:20:54 -0500

    [Create] more constructor

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Base class of a Zombie enemy</summary>
+    public class Zombie
+    {
+        /// <summary>Field for Zombie health</summary>
+        public int health;
+
+        /// <summary>Constructor for Class <see cref="Zombie"/></summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>Constructor for Class <see cref="Zombie"/> with argument</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                this.health = value;
+        }
+    }
+}
