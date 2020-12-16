4148c1de0af28c1821a1e7a0cdec1ea69488685c
Mitchell <mmoscovics@yahoo.com>
2020-12-15 23:32:56 -0500

    [Create] name

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Base class of a Zombie enemy</summary>
+    public class Zombie
+    {
+        /// <summary>Field for Zombie health</summary>
+        private int health;
+        /// Field for Zombie name
+        private string name = "(No name)";
+
+        /// Property getter and setter for name
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        /// <summary>Constructor for Class <see cref="Zombie"/></summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>Constructor for Class <see cref="Zombie"/> with argument</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                this.health = value;
+        }
+
+        /// <summary>Method to get Zombie health</summary>
+        /// <return>Value of health</return>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
