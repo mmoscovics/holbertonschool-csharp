d5dd3bce3497942c790885fc4eb905c020e2de02
Mitchell <mmoscovics@yahoo.com>
2020-12-15 23:17:28 -0500

    [Create] class with field

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Base class of a Zombie enemy</summary>
+    public class Zombie
+    {
+        /// <summary>Field for Zombie health</summary>
+        public int health;
+
+        /// <summary>Constructor for Class <see cref="Zombie"/></summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
