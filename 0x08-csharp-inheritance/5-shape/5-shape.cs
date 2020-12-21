5a73b24a73f21edaa10ebef3f7cc8bad17eb2217
Mitchell <mmoscovics@yahoo.com>
2020-12-21 16:04:46 -0500

    [Create] exception

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// Base class
+class Shape
+{
+    /// throw exception
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
