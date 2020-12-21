ae8e43f7d15e1d8a18a604beffc9cce096817e23
Mitchell <mmoscovics@yahoo.com>
2020-12-21 15:36:36 -0500

    [Create] get properties and methods

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+/// Base Obj class
+class Obj
+{
+    /// Prints name of available properties and methods
+    public static void Print(object myObj)
+    {
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach (var prop in myObj.GetType().GetProperties())
+            Console.WriteLine(prop.Name);
+
+        Console.WriteLine($"{myObj.GetType().Name} methods:");
+        foreach (var meth in myObj.GetType().GetMethods())
+            Console.WriteLine(meth.Name);
+    }
+}
