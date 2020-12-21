88273be7576aadaed34b72c91f0342969830c7d9
Mitchell <mmoscovics@yahoo.com>
2020-12-21 15:16:28 -0500

    [Create] check if array

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// Base Obj class
+class Obj
+{
+    /// Check if obj is an instance of or inherited from Array
+    /// Returns true or false
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType().IsArray)
+            return true;
+        return false;
+    }
+}
