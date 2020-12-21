255b27617a57097bae324b8732b13c654431e1a2
Mitchell <mmoscovics@yahoo.com>
2020-12-21 15:06:55 -0500

    [Create] check if int

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// Base Obj class
+class Obj
+{
+    /// Check if obj is int, return true or false
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
