a1e3798dee32c03f8ff2bc0c0fd09eb9f9e9e379
Mitchell <mmoscovics@yahoo.com>
2020-12-21 15:24:56 -0500

    [Create] check is subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// Base Obj class
+class Obj
+{
+    /// Check if obj is instances or inherits from subclass
+    /// Returns true or false
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
