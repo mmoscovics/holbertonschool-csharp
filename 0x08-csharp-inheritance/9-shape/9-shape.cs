a6606e39eefbd78dedf299d2fc3e89fcfc9adb3b
Mitchell <mmoscovics@yahoo.com>
2020-12-22 00:05:52 -0500

    [Create] more override

---
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+/// Base class
+class Shape
+{
+    /// throw exception
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// Rectangle ingerits from Shape
+class Rectangle : Shape
+{
+    /// Private fields
+    private int width;
+    private int height;
+
+    /// Public properties with getter and setter
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+
+    /// Area override
+    public override int Area()
+    {
+        return width * height;
+    }
+
+    /// String override
+    public override string ToString()
+    {
+        return ($"[Rectangle] {width} / {height}");
+    }
+}
+
+/// Square inherits from Rectangle
+class Square : Rectangle
+{
+    /// Private fields
+    private int size;
+
+    /// Public properties with getter and setter
+    public int Size
+    {
+        get { return size; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            size = value;
+            Width = value;
+            Height = value;
+        }
+    }
+
+    /// String override
+    public override string ToString()
+    {
+        return ($"[Square] {size} / {size}");
+    }
+}
