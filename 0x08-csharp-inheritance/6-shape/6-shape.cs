ad6bd1fabca6dc19f114ea1beed7d24929700dfa
Mitchell <mmoscovics@yahoo.com>
2020-12-21 23:37:16 -0500

    [Create] getter and setter

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// Base class
+class Shape
+{
+    /// throw exception
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// Rectangle ingerits from Shape
+class Rectangle : Shape
+{
+    /// Private fields
+    private int width;
+    private int height;
+
+    /// Public properties with getter and setter
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0.");
+            width = value;
+        }
+    }
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0.");
+            height = value;
+        }
+    }
+}
