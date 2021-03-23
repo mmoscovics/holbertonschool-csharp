fcb2ece99f7398170ade2f31c84ea0235ea022b8
Mitchell <mmoscovics@yahoo.com>
2021-03-22 23:32:37 -0400

    [Create] generics dequeue

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+/// Queue class ///
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count = 0;
+    
+    /// Node class ///
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+        public Node(T val)
+        {
+            value = val;
+        }
+    }
+
+    /// Check Queue's type ///
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// Returns number of nodes in the Queue ///
+    public int Count()
+    {
+        return count;
+    }
+
+    /// Creates new node and adds it to end of queue and update count ///
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+            head = newNode;
+        if (tail != null)
+            tail.next = newNode;
+        tail = newNode;
+
+        count++;
+    }
+
+    /// Removes first node in the queue and returns its value ///
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            T value = head.value;
+            head = head.next;
+            count--;
+            return value;
+        }
+    }    
+}
