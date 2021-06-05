b14229ac23a0315a0d56a6ebff42e494f57bcd37
Mitchell <mmoscovics@yahoo.com>
2021-06-04 22:53:37 -0400

    [Create] inventory

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/InventoryLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+/// BaseClass ///
+    class BaseClass
+    {
+        public string id{get; set;}
+        public DateTime date_created{get; set;}
+        public DateTime date_updated{get; set;}
+    }
+}
