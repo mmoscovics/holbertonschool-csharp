aaeea24379d3413489695481029a312d3096456b
Mitchell <mmoscovics@yahoo.com>
2021-06-04 22:59:01 -0400

    [Update]

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+/// BaseClass ///
+    public class BaseClass
+    {
+        public string id;
+        public DateTime date_created;
+        public DateTime date_updated;
+    }
+}
